a1724a81b5ebdab07fe4147f71f4038d792b9b8b
SoloPane <sabinacrp88@gmail.com>
2022-08-10 12:33:11 +0600

    punkshiba

---
+++ precache-manifest.11e9f26ca2335a39f931cf11ef9adbf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8111f0e24e335712e0d70e595e05e72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7811b5c748b924df7a6b",
+    "url": "/static/css/main.679f8cee.chunk.css"
+  },
+  {
+    "revision": "4722b07854ad563178cb",
+    "url": "/static/js/2.ceefd06a.chunk.js"
+  },
+  {
+    "revision": "7811b5c748b924df7a6b",
+    "url": "/static/js/main.f8765f18.chunk.js"
+  },
+  {
+    "revision": "9b0a0633692965e73460",
+    "url": "/static/js/runtime~main.26814d88.js"
+  }
+]);
